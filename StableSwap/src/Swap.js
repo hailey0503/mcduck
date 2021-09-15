2119399240eb0ed102604b6728271c38c6809525
HaileyPong <haileypong@berkeley.edu>
2021-09-14 18:43:36 -0700

    add swap box

--- StableSwap/src/Swap.js
+++ StableSwap/src/Swap.js
@@ -14,6 +14,10 @@ import InputGroup from 'react-bootstrap/InputGroup'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 
 
 const mainnet = {
@@ -40,42 +44,72 @@ function Swap() {
   
 	return (
 		<container>
-		
-			<div>
-				<Card className="text-center">
-					<Card.Header>Swap your stable coins</Card.Header>
-						<Card.Body>
-							<Card.Title>Special title treatment</Card.Title>
-							
-					<InputGroup className="mb-3">
-						<DropdownButton
-							variant="outline-secondary"
-							title="From"
-							id="input-group-dropdown-1"
-						>
-						<Dropdown.Item href="#">UST</Dropdown.Item>
-						<Dropdown.Item href="#">USDT</Dropdown.Item>
-						<Dropdown.Item href="#">USCD</Dropdown.Item>
-						</DropdownButton>
-						<FormControl aria-label="Text input with dropdown button" />
-					</InputGroup>
+			<div class="container">
+        		<div class="row">
+            		<div class="col-md-8 col-lg-4 B">
+                		<div class="card card-inverse card-primary">
+                   
+                		</div>
+           		</div>
+            <div class="col-md-4 col-lg-3 G">
+                <div class="row h-100">
+                    <div class="col-md-6 col-lg-6 B h-50 pb-3">
+                        <div class="card card-inverse card-success h-100">
 
-					<InputGroup className="mb-3">
-						<DropdownButton
-							variant="outline-secondary"
-							title="To"
-							id="input-group-dropdown-1"
-						>
-						<Dropdown.Item href="#">UST</Dropdown.Item>
-						<Dropdown.Item href="#">USDT</Dropdown.Item>
-						<Dropdown.Item href="#">USCD</Dropdown.Item>
-						</DropdownButton>
-						<FormControl aria-label="Text input with dropdown button" />
-					</InputGroup>
-						</Card.Body>
-						<Card.Footer className="text-muted">2 days ago</Card.Footer>
-				</Card>
-			</div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6 B h-50 pb-3">
+                        <div class="card card-inverse bg-success h-100">
+
+                        </div>
+                    </div>
+                    <div class="col-md-12 h-50">
+                        <div class="card card-inverse bg-danger h-200">
+							<div>
+								<Card className="text-center">
+									<Card.Header>Swap your stable coins</Card.Header>
+										<Card.Body>
+											<Card.Title></Card.Title>
+											
+									<InputGroup className="mb-3">
+										<DropdownButton
+											variant="outline-secondary"
+											title="From"
+											id="input-group-dropdown-1"
+										>
+										<Dropdown.Item href="#">UST</Dropdown.Item>
+										<Dropdown.Item href="#">USDT</Dropdown.Item>
+										<Dropdown.Item href="#">USCD</Dropdown.Item>
+										</DropdownButton>
+										<FormControl aria-label="Text input with dropdown button" />
+									</InputGroup>
+
+									<InputGroup className="mb-3">
+										<DropdownButton
+											variant="outline-secondary"
+											title="To"
+											id="input-group-dropdown-1"
+										>
+										<Dropdown.Item href="#">UST</Dropdown.Item>
+										<Dropdown.Item href="#">USDT</Dropdown.Item>
+										<Dropdown.Item href="#">USCD</Dropdown.Item>
+										</DropdownButton>
+										<FormControl aria-label="Text input with dropdown button" />
+									</InputGroup>
+										</Card.Body>
+										<Card.Footer className="text-muted">
+										<Button variant="outline-success">Swap</Button>{' '}
+										</Card.Footer>
+								</Card>
+							</div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+			
 		</container>
 	);
   }
